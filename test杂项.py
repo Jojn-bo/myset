eece85a4572a4b9c35f30f48cc48d07208cc2229
hasee <740416094@qq.com>
2017-11-11 17:19:19 +0800

    t

--- test杂项.py
+++ test杂项.py
@@ -5,8 +5,8 @@ import random
 i=0
 while i < 10:
     # print(random.choice([5,'a','/',3]))
-    # print(random.choice('A String'))
-    print(random.randint(0,9))
+    print(random.choice('A String'))
+    #print(random.randint(0,9))
     i+=1
 
 
