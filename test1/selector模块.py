2bcaf9cd2cddee5611ba3c85cd80c362d8913610
bo <740416094@qq.com>
2017-11-17 17:41:39 +0800

    a

---
+++ test1/selector模块.py
@@ -0,0 +1,32 @@
+import selectors
+import socket
+
+sel = selectors.DefaultSelector()
+
+def accept(sock, mask):
+    conn, addr = sock.accept() #Should be ready
+    print('accepted', conn, 'from', addr, mask)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)#新链接注册read回调函数
+
+def read(conn, mask):
+    data = conn.recv(1024) #should be ready
+    if data:
+        print('echoing', repr(data), 'to', conn)
+        conn.send(data)
+    else:
+        print('closing', conn)
+        sel.unregister(conn)
+        conn.close()
+
+sock = socket.socket()
+sock.bind(('localhost',10001))
+sock.listen(100)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, accept)
+
+while True:
+    events = sel.select() #默认阻塞，有活动连接就返回活动的连接列表
+    for key, mask in events:
+        callback = key.data #accept
+        callback(key.fileobj, mask) #key.fileobj = 文件句柄
