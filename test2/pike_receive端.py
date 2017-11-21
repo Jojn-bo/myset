0be7a3f11a382f2eb39bfafce3ec73a2f4f28908
bo <740416094@qq.com>
2017-11-21 17:59:48 +0800

    a

--- test2/pike_receive端.py
+++ test2/pike_receive端.py
@@ -1,20 +1,25 @@
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 #这个声明是防止先运行消费者.py时,没声明队列引起报错而再声明一次的，
 #如果确定生产者.py中有这个队列，则不用声明（再声明一次也没什么关系）
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello',durable=True)
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body):#回调函数
     print('-->',ch, method, properties)
+    #time.sleep(30)
     print("[x] Received %r"% body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)#手动发信息给服务器确认
 
+channel.basic_qos(prefetch_count=1)#如果服务器没处理完，就把信息发送到空闲的服务器上
 channel.basic_consume(#消费消息
                     callback,#如果收到消息，就调用callback函数来处理
                     queue='hello',
-                    no_ack=True)
+                    #no_ack=True #消息处理完确认
+)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- test2/pike_send端.py
+++ test2/pike_send端.py
@@ -4,12 +4,14 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()#声明一个管道
 
 #声明queue
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='hello',durable=True)#durable服务器down了，可以持久化消息队列
 
 # n RabbitMQ a message can never be sent directly to the queue, it away
 channel.basic_publish(exchange='',
                       routing_key='hello',#queue名字
-                      body='Hello World!')
+                      body='Hello World!',
+                      properties=pika.BasicProperties(delivery_mode=2)#让队列中的消息持久化
+                      )
 
 print("[x] Sent 'Hello World!'")
 connection.close()#关闭队列
