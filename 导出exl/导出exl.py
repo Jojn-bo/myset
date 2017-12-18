b5cd7d9e89bc343536ab7ccb8cd6cceab7226f2f
bo <740416094@qq.com>
2017-12-18 17:31:24 +0800

    2017-12-18 change

--- 导出exl/导出exl.py
+++ 导出exl/导出exl.py
@@ -8,17 +8,29 @@ import re
 # driver.find_element_by_id('txtPassword').send_keys('-0')
 # driver.find_element_by_id('btnLogin').click()
 
-
+# 打开测试文档
 with open('测试文档', encoding='utf-8') as f:#加encoding的原因是因为测试文档中，有unicode
     msg = f.read()
 
-# print(type(msg))
-space = re.findall('\s*', msg) #找到所有的空格
-# print(type(space))
-space=''.join(space)
-msg1 = msg.replace(space, '') #把所有的空格用去除
-print(msg1)
-# invoice_code = re.search('\d{10}', msg1)
-# print(invoice_code[0])
-# invoice_num = re.findall('\d{8}', msg1)
-# print(invoice_num[1])
+# 正则搜索关键信息
+invoiceCode = re.search('\d{10}', msg)[0]  # 发票代码
+# print(invoiceCode)
+invoiceNum = re.findall('\d{8}', msg)  # 发票号码
+# print(invoiceNum[1])
+date = re.search('\d{4}年\d{2}月\d{2}', msg)[0]  # 开票日期
+# print(date)
+date = date.replace('年', '-')
+date = date.replace('月', '-')
+str=' 00:00:00'
+date = date + str
+# print(date)
+# 开始匹配商品
+commodity = re.search('(税额([\s\S]*)\n合计)', msg)[0]
+# 如果有很多商品
+commodity1 = re.findall('(\n([\s\S]*)\n)?', commodity)[3]
+print(commodity1[3])
+
+# 把相关信息放入excel
+
+
+# 保存excel
